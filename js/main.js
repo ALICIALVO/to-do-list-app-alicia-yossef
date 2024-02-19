a74178ea9cbb3a552bfb8fd67d51fc76401a65bf
Alicia Lvovsky <alicialevovsky@gmail.com>
2024-02-19 15:14:20 +0200

    created filterTasks function

--- js/main.js
+++ js/main.js
@@ -1,65 +1,69 @@
-const genID = ()=> Math.random().toString(36).slice(2,8);
+const genID = () => Math.random().toString(36).slice(2, 8);
 const todoList = document.querySelector('.todo-list');
 const taskInput = document.querySelector('#task-input');
 const addButton = document.querySelector('.add-task');
 const todoForm = document.querySelector('.todo-form');
 const clearCompletedBtn = document.querySelector('.clear-completed');
 const filterButtons = document.querySelector('.filter-btns')
+const tasksLeft = document.querySelector('.tasks-left')
 
 let todoData = [];
 
-function onFormSubmit(event){
-    event.preventDefault(); 
-    addTask(); 
+function onFormSubmit(event) {
+    event.preventDefault();
+    addTask();
 }
 
 todoForm.addEventListener('submit', onFormSubmit);
 
-
-function addTask(){
-    if(taskInput.value === ''){
+let counterTask = 0;
+function addTask() {
+    if (taskInput.value === '') {
         alert('Hey there, you forgot to write something!(:');
-        
-    }else{
 
-    // gets value from user input:
-    const inputValue = taskInput.value;
+    } else {
 
-    // creates li:
-    const todoTask = document.createElement('li');
-    todoTask.classList.add('todo-task');
-    todoTask.classList.add("active");
+        counterTask++
+        tasksLeft.textContent = counterTask + " tasks left"
+        // gets value from user input:
+        const inputValue = taskInput.value;
 
-    // creates checkbox:
-    const checkBox = document.createElement('input');
-    checkBox.type = 'checkbox';
-    checkBox.classList.add('task-checkbox');
+        // creates li:
+        const todoTask = document.createElement('li');
+        todoTask.classList.add('todo-task');
+        todoTask.classList.add("active");
 
-    // creates label:
-    const label = document.createElement('label');
-    label.textContent = inputValue;
-    label.classList.add('task-label');
+        // creates checkbox:
+        const checkBox = document.createElement('input');
+        checkBox.type = 'checkbox';
+        checkBox.classList.add('task-checkbox');
 
-    // creates removeTask button:
-    const removeTask = document.createElement('button');
-    removeTask.innerHTML = '<i class="fa-regular fa-trash-can" style="color: #71385c;"></i>';
-    removeTask.classList.add('task-remove');
+        // creates label:
+        const label = document.createElement('label');
+        label.textContent = inputValue;
+        label.classList.add('task-label');
 
-    todoTask.appendChild(checkBox);
-    todoTask.appendChild(label);
-    todoTask.appendChild(removeTask);
+        // creates removeTask button:
+        const removeTask = document.createElement('button');
+        removeTask.innerHTML = '<i class="fa-regular fa-trash-can" style="color: #71385c;"></i>';
+        removeTask.classList.add('task-remove');
 
-    todoList.appendChild(todoTask);
-    // added push to array with task = inputValue , status to active , 
-    // and id calls a function that generates a rundom unique number.
-    todoData.push({task : taskInput.value,
-                    status : "active",
-                    id : genID()});
-    console.log(todoData)
-    taskInput.value = '';
-    // saveData();
-    
-    removeTask.addEventListener('click', deleteTask);
+        todoTask.appendChild(checkBox);
+        todoTask.appendChild(label);
+        todoTask.appendChild(removeTask);
+
+        todoList.appendChild(todoTask);
+        // added push to array with task = inputValue , status to active , 
+        // and id calls a function that generates a rundom unique number.
+        todoData.push({
+            task: taskInput.value,
+            status: "active",
+            id: genID()
+        });
+        taskInput.value = '';
+        // saveData();
+
+        removeTask.addEventListener('click', deleteTask);
     }
 }
 
@@ -71,11 +75,17 @@ function taskCompletion(event) {
     if (event.target.matches('.task-checkbox')) {
         let closetsLi = event.target.closest('li');
         if (event.target.checked) {
+
             closetsLi.classList.add("completed");
-            closetsLi.classList.remove("active");  
+            closetsLi.classList.remove("active");
+            counterTask--
+            tasksLeft.textContent = counterTask + " tasks left"
+
         } else {
             closetsLi.classList.remove("completed");
             closetsLi.classList.add("active");
+            counterTask++
+            tasksLeft.textContent = counterTask + " tasks left"
             // for(let todo of todoData){
             //     if(closetsLi.matches('.active')){
             //         todo.status = "active";                    
@@ -91,10 +101,11 @@ todoList.addEventListener("click", taskCompletion);
 
 
 // deletes tasks with trash icon:
-function deleteTask(event){
+function deleteTask(event) {
     const closetsLi = event.target.closest('li');
     closetsLi.remove();
-    console.log(todoData);
+    counterTask--
+    tasksLeft.textContent = counterTask + " tasks left";
 }
 // todoList.addEventListener('click', deleteTask);deletes all-not working.
 
@@ -110,38 +121,45 @@ function clearCompletedTasks() {
 }
 clearCompletedBtn.addEventListener('click', clearCompletedTasks);
 
-function filterTasks(event){
+function filterTasks(event) {
     const activeTasks = document.querySelectorAll('.active')
     const completedTasks = document.querySelectorAll('.completed');
 
-    if(event.target.matches('.filter-all')){
+
+    if (event.target.matches('.filter-all')) {
         for (let i = 0; i < completedTasks.length; i++) {
             completedTasks[i].style.display = "flex";
         }
         for (let i = 0; i < activeTasks.length; i++) {
             activeTasks[i].style.display = "flex";
+            // tasksLeft.textContent = i + 1 + " tasks left"
         }
     }
 
-    if(event.target.matches('.filter-active')){
+    if (event.target.matches('.filter-active')) {
         for (let i = 0; i < completedTasks.length; i++) {
             completedTasks[i].style.display = "none";
         }
         for (let i = 0; i < activeTasks.length; i++) {
             activeTasks[i].style.display = "flex";
+            tasksLeft.textContent = i + 1 + " tasks left"
         }
     }
 
-    if(event.target.matches('.filter-completed')){
+    if (event.target.matches('.filter-completed')) {
         for (let i = 0; i < activeTasks.length; i++) {
             activeTasks[i].style.display = "none";
         }
         for (let i = 0; i < completedTasks.length; i++) {
             completedTasks[i].style.display = "flex";
+       
+            // counterTask
+            // tasksLeft.textContent = counterTask + " task completed"
+            tasksLeft.textContent = i + 1 + " task completed";
         }
     }
-    
-    
+
+
 
     // if(event.target.matches('.filter-all')){   
     //     for (let i = 0; i < allTasks.length; i++) {
@@ -151,7 +169,7 @@ function filterTasks(event){
 
 
 }
-filterButtons.addEventListener('click' ,filterTasks)
+filterButtons.addEventListener('click', filterTasks)
 // function saveData(){
 //     localStorage.setItem("data", todoList.innerHTML);
 // }
